6449965a6faf616238fc9eeb0dae1e3a2e441e0b
ando0311 <ando@mediaxis.jp>
2017-08-18 19:13:18 +0900

    first commit

---
+++ LendingManagement/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  LendingManagement
+//
+//  Created by 安藤一心 on 2017/08/18.
+//  Copyright © 2017年 Isshin Ando. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ LendingManagementTests/LendingManagementTests.swift
@@ -0,0 +1,36 @@
+//
+//  LendingManagementTests.swift
+//  LendingManagementTests
+//
+//  Created by 安藤一心 on 2017/08/18.
+//  Copyright © 2017年 Isshin Ando. All rights reserved.
+//
+
+import XCTest
+@testable import LendingManagement
+
+class LendingManagementTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
